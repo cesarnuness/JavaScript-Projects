374414e78f01f7a8bcaab3b5d6c78c6facf9adfa
cesarnuness <cesar.nunes@live.com>
2022-05-29 15:31:20 -0300

    Finished basic project

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -2,11 +2,16 @@
 
 const container = document.querySelector('.container');
 
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
 function divCreator() {
     for (let i = 0; i < 256; i++) {
         const piece = document.createElement('div');
         piece.className = `square piece${i}`;
-        console.log(i);
+        piece.addEventListener('mouseover', changeBackground)
+        piece.addEventListener('mousedown', changeBackground)
         container.append(piece);
     }
 }
@@ -16,6 +21,7 @@ const getRandomNumber = (maxNum) => {
     return Math.floor(Math.random() * maxNum);
 };
 
+
 const getRandomColor = () => {
     const h = getRandomNumber(360);
     const s = getRandomNumber(100);
@@ -24,10 +30,8 @@ const getRandomColor = () => {
 };
 
 function changeBackground(e) {
-    if (e.type === 'mouseover' && !mousedown) return
+    console.log('teste');
+    if (e.type === 'mouseover' && !mouseDown) return
     const randomColor = getRandomColor();
     e.target.style.backgroundColor = randomColor
 }
-
-const square = Array.from(document.querySelectorAll('.square'));
-square.forEach(square => square.addEventListener('onmousedown', changeBackground))
