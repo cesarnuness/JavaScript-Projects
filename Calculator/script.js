7b561ec7667650c9debf794de20017b2cada1269
cesarnuness <cesar.nunes@live.com>
2022-06-28 00:02:18 -0300

    Finished Javascript! Will update one I learn grid!

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,35 +1,100 @@
 "use strict";
-
+//Initial Values
 const calculationState = {
   firstNumber: 0,
   secondNumber: 0,
   operation: null,
 };
 
+// basic functions used in the calculator
+const clear = function () {};
+const sum = function (a, b) {
+  return a + b;
+};
+const subtraction = function (a, b) {
+  return a - b;
+};
+const multiply = function (a, b) {
+  return a * b;
+};
+const division = function (a, b) {
+  return a / b;
+};
+
+//Main selectors
 const number = document.querySelectorAll(".data-number");
 const operator = document.querySelectorAll(".data-operator");
 const equal = document.querySelector(".data-equal");
-
 const display = document.querySelector(".display");
+const topDisplay = document.querySelector(".top-display");
+const deleteLast = document.querySelector(".delete");
+const clearAll = document.querySelector(".clear");
 
 for (let i = 0; i < number.length; i++) {
-  number[i].addEventListener("click", function (e) {
+  number[i].addEventListener("click", function () {
     display.innerHTML += this.innerHTML;
   });
 }
 
-function sum(a, b) {
-  return a + b;
-}
+const deleteLastNumber = function () {
+  display.innerHTML = display.innerHTML.slice(0, -1);
+};
 
-function subtraction(a, b) {
-  return a - b;
-}
+const clearAllAfter = function () {
+  display.innerHTML = "";
+  calculationState.firstNumber = 0;
+  calculationState.secondNumber = 0;
+  calculationState.operation = null;
+};
 
-function multiplication(a, b) {
-  return a * b;
-}
+const sumAfter = function () {
+  calculationState.firstNumber = parseInt(display.innerHTML);
+  calculationState.operation = sum;
+  display.innerHTML = "";
+  topDisplay.innerHTML = `${calculationState.firstNumber} +`;
+};
 
-function division(a, b) {
-  return a / b;
-}
+const subtractionAfter = function () {
+  calculationState.firstNumber = parseInt(display.innerHTML);
+  calculationState.operation = subtraction;
+  display.innerHTML = "";
+  topDisplay.innerHTML = `${calculationState.firstNumber} -`;
+};
+
+const multiplyAfter = function () {
+  calculationState.firstNumber = parseInt(display.innerHTML);
+  calculationState.operation = multiply;
+  display.innerHTML = "";
+  topDisplay.innerHTML = `${calculationState.firstNumber} *`;
+};
+
+const divisionAfter = function () {
+  calculationState.firstNumber = parseInt(display.innerHTML);
+  calculationState.operation = division;
+  display.innerHTML = "";
+  topDisplay.innerHTML = `${calculationState.firstNumber} /`;
+};
+
+const equalize = function () {
+  calculationState.secondNumber = parseInt(display.innerHTML);
+  display.innerHTML = calculationState.operation(
+    calculationState.firstNumber,
+    calculationState.secondNumber
+  );
+  topDisplay.innerHTML = display.innerHTML;
+  display.innerHTML = "";
+};
+
+equal.addEventListener("click", equalize);
+
+const sumOperation = document.querySelector(".sum");
+const subOperation = document.querySelector(".minus");
+const multOperation = document.querySelector(".multiply");
+const divOperation = document.querySelector(".division");
+
+sumOperation.addEventListener("click", sumAfter);
+subOperation.addEventListener("click", subtractionAfter);
+multOperation.addEventListener("click", multiplyAfter);
+divOperation.addEventListener("click", divisionAfter);
+deleteLast.addEventListener("click", deleteLastNumber);
+clearAll.addEventListener("click", clearAllAfter);
