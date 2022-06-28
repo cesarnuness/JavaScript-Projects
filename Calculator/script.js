b03e0ce07f0d44c2d00618f33a8803620b666c51
cesarnuness <cesar.nunes@live.com>
2022-06-27 23:14:26 -0300

    Starting dom manipulation

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,35 @@
-'use strict';
+"use strict";
+
+const calculationState = {
+  firstNumber: 0,
+  secondNumber: 0,
+  operation: null,
+};
+
+const number = document.querySelectorAll(".data-number");
+const operator = document.querySelectorAll(".data-operator");
+const equal = document.querySelector(".data-equal");
+
+const display = document.querySelector(".display");
+
+for (let i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function (e) {
+    display.innerHTML += this.innerHTML;
+  });
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
