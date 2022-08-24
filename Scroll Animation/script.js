9477a16f4254178a96b99e8ea498c9474cea8d78
cesarnuness <cesar.nunes@live.com>
2022-08-24 12:45:49 -0300

    Scroll Animation Finished

---
+++ Scroll Animation/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+function checkBoxes() {
+  boxes.forEach((box) => {
+    if (box.getBoundingClientRect().top < window.innerHeight) {
+      box.classList.add("show");
+    }
+  });
+}
