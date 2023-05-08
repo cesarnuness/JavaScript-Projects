06d4cfc06e0426877af623154dbb32639beb2790
cesarnuness <cesar.nunes@live.com>
2023-05-07 23:47:09 -0300

    Finished Displaying Results after Query

--- forkify/src/js/controller.js
+++ forkify/src/js/controller.js
@@ -2,10 +2,11 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   recipeView.renderSpinner();
@@ -23,7 +24,25 @@ const controlRecipes = async function () {
 };
 controlRecipes();
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Load search results
+    await model.loadSearchResults(query);
+
+    // Render results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
